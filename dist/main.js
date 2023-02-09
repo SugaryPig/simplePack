3b194d4064047037a4dd5bb0b299cd5622bc923b
陈婷 <chenting@192.168.1.5>
2023-02-10 00:15:21 +0800

    code review

--- dist/main.js
+++ dist/main.js
@@ -1,31 +1,24 @@
-(function (modules) {
-  function require(fileName) {
-    debugger
-    const fn = modules[fileName]
+(function(modules){
+      function require(fileName) {
+        const fn = modules[fileName]
 
-    const module = { exports: {} }
+        const module = { exports:{} }
 
-    fn(require, module, module.exports)
+        fn(require, module, module.exports)
 
-    return module.exports
-  }
-  require('/Users/chenting/Desktop/simplePack/src/index.js')
-})({
-  '/Users/chenting/Desktop/simplePack/src/index.js': function (require, module, exports) {
-    "use strict";
+        return module.exports
+      }
+      require('/Users/chenting/Desktop/simplePack/src/index.js')
+    })({'/Users/chenting/Desktop/simplePack/src/index.js': function(require, module, exports) { "use strict";
 
-    var _greeting = require("./greeting.js");
+var _greeting = require("./greeting.js");
 
-    document.write((0, _greeting.greeting)('tingting'));
-  }, './greeting.js': function (require, module, exports) {
-    "use strict";
+document.write((0, _greeting.greeting)('tingting')); },'./greeting.js': function(require, module, exports) { "use strict";
 
-    Object.defineProperty(exports, "__esModule", {
-      value: true
-    });
-    exports.greeting = greeting;
-    function greeting(name) {
-      return 'hello ' + name;
-    }
-  },
-})
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.greeting = greeting;
+function greeting(name) {
+  return 'hello ' + name;
+} },})
