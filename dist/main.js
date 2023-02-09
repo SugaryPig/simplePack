6a389d045cbcd1b6a84e794c1e4a1285137603e7
陈婷 <chenting@192.168.1.5>
2023-02-10 00:09:29 +0800

    feat(simplepack): 实现一个简易simplepack

---
+++ dist/main.js
@@ -0,0 +1,31 @@
+(function (modules) {
+  function require(fileName) {
+    debugger
+    const fn = modules[fileName]
+
+    const module = { exports: {} }
+
+    fn(require, module, module.exports)
+
+    return module.exports
+  }
+  require('/Users/chenting/Desktop/simplePack/src/index.js')
+})({
+  '/Users/chenting/Desktop/simplePack/src/index.js': function (require, module, exports) {
+    "use strict";
+
+    var _greeting = require("./greeting.js");
+
+    document.write((0, _greeting.greeting)('tingting'));
+  }, './greeting.js': function (require, module, exports) {
+    "use strict";
+
+    Object.defineProperty(exports, "__esModule", {
+      value: true
+    });
+    exports.greeting = greeting;
+    function greeting(name) {
+      return 'hello ' + name;
+    }
+  },
+})
